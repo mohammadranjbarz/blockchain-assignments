ac181bb23e327f8a0dd2cebfaa1c4752133d173c
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-11-15 18:02:00 +0300

    Generate key pair with secret key

--- assignment2/src/main.py
+++ assignment2/src/main.py
@@ -14,7 +14,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 
 
-def generate_private_key():
+def generate_private_key(private_key_pass):
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=4096,
@@ -23,7 +23,7 @@ def generate_private_key():
     private_pem = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+        encryption_algorithm=serialization.BestAvailableEncryption(bytes(private_key_pass, 'utf-8'))
     )
     public_key = private_key.public_key()
     public_pem = public_key.public_bytes(
@@ -33,9 +33,9 @@ def generate_private_key():
     return private_pem, public_pem
 
 
-def sign(private_key_pem, message):
+def sign(private_key_pem,private_key_pass, message):
     private_key = serialization.load_pem_private_key(
-        private_key_pem, password=None, backend=default_backend())
+        private_key_pem, password=bytes(private_key_pass, 'utf-8'), backend=default_backend())
     prehashed = hashlib.sha256(bytes(message, 'utf-8')).hexdigest()
     sig = private_key.sign(
         bytes(prehashed.encode('ascii')),
@@ -67,15 +67,16 @@ def verify(public_key_pem, message, signed):
         return False
 
 
-keys = generate_private_key()
+private_key_pass = 'key secret'
+keys = generate_private_key(private_key_pass)
 
-print('keys')
+# print('keys')
 # print(keys[0].decode("utf-8"))
 # print(keys[1].decode("utf-8"))
 
 print('**********')
 plain_text = 'hey you'
-signed = sign(keys[0], plain_text)
+signed = sign(keys[0],private_key_pass, plain_text)
 print('signed')
 print(signed)
 print('\n**********\n')
